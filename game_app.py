894271d2340e43104b54bba65e0725ed33b50f71
Stefan Boos <kontakt@boos.systems>
2025-08-16 16:53:28 +0200

    feat: living cell without any neighbour dies

--- game_app.py
+++ game_app.py
@@ -4,7 +4,7 @@ from textual.widgets import Log, Footer, Header
 
 class GameApp(App):
     CSS_PATH = "style.tcss"
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"),("n", "produce_next_generation", "Next generation")]
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -14,11 +14,22 @@ class GameApp(App):
     def on_ready(self) -> None:
         log = self.query_one(Log)
         log.write_line(" 123456789")
-        log.write_line("1█ █ █ █ █1")
-        log.write_line("2 █ █ █ █ 2")
-        log.write_line("3█ █ █ █ █3")
-        log.write_line("4 █ █ █ █ 4")
-        log.write_line("5█ █ █ █ █5")
+        log.write_line("1         1")
+        log.write_line("2   █     2")
+        log.write_line("3         3")
+        log.write_line("4         4")
+        log.write_line("5         5")
+        log.write_line(" 123456789")
+
+    def action_produce_next_generation(self) -> None:
+        log = self.query_one(Log)
+        log.clear()
+        log.write_line(" 123456789")
+        log.write_line("1         1")
+        log.write_line("2         2")
+        log.write_line("3         3")
+        log.write_line("4         4")
+        log.write_line("5         5")
         log.write_line(" 123456789")
 
     def action_toggle_dark(self) -> None:
--- test_game_app.py
+++ test_game_app.py
@@ -1,3 +1,6 @@
 def test_game_app(snap_compare):
     assert snap_compare('game_app.py')
 
+def test_n_key_should_produce_next_generation(snap_compare):
+    assert snap_compare("game_app.py", press=["n"])
+
